1421b0c3a927e59eab854708a1ab7cb12e7963a7
MaryaLauraRS <marya.santos@estudante.ifms.edu.br>
2023-08-25 14:29:53 -0400

    comit

---
+++ src/Todo/ToDo.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function ToDo() {
+    const [atividade,setAtividade] = useState("");
+    const [lista,setLista] = useState ([]);
+    const[id,setId] = useState (1);
+    const salvar = (a) => {
+        a.preventDefault();
+        setLista([...lista, {
+            id: id,
+            atividade: atividade
+        }]);
+        setId(id+1);
+    }
+    
+    return (
+    <div>
+    <Link to="/">home</Link>
+        <h1>Lista de Atividades</h1>
+         <form onSubmit={salvar}>
+        <input type="text" onChange={(e)=>{setAtividade(e.target.value)}}/>
+        <button>ADD</button>
+        </form>   
+            <div>
+                {lista.map((atividade) => <p key={atividade.id}>  {atividade.atividade}</p>)}
+            </div>
+           
+        </div>
+    );
+}
